775c66193caedbd969e8e2950f94c0401f7cf114
Alexander Mancevice <amancevice@cargometrics.com>
2016-01-28 22:05:31 -0500

    migration

--- lib/engine.rb
+++ lib/engine.rb
@@ -19,35 +19,35 @@ require "engine/models/truck"
 require "engine/weekday"
 
 module Engine
-  def self.migrate
-    migration = ActiveRecord::Migration.new
+  class Migration < ActiveRecord::Migration
+    def change
+      create_table :places do |t|
+        t.float  :latitude
+        t.float  :longitude
+        t.string :city
+        t.string :cross
+        t.string :main
+        t.string :name
+        t.string :neighborhood
+        t.string :number
+        t.string :source
+        t.string :street
+        t.string :type
+      end
 
-    migration.create_table :places do |t|
-      t.float  :latitude
-      t.float  :longitude
-      t.string :city
-      t.string :cross
-      t.string :main
-      t.string :name
-      t.string :neighborhood
-      t.string :number
-      t.string :source
-      t.string :street
-      t.string :type
-    end
+      create_table :trucks do |t|
+        t.string :city
+        t.string :name
+        t.string :site
+        t.string :source
+      end
 
-    migration.create_table :trucks do |t|
-      t.string :city
-      t.string :name
-      t.string :site
-      t.string :source
-    end
-
-    migration.create_table :patterns do |t|
-      t.belongs_to :place
-      t.belongs_to :truck
-      t.string     :type
-      t.string     :value
+      create_table :patterns do |t|
+        t.belongs_to :place
+        t.belongs_to :truck
+        t.string     :type
+        t.string     :value
+      end
     end
   end
 
