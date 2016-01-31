6b2dda5434b44c7803f22cccef356d0019e9e716
Alexander Mancevice <amancevice@cargometrics.com>
2016-01-31 15:43:30 -0500

    client query

--- lib/engine/client.rb
+++ lib/engine/client.rb
@@ -7,14 +7,19 @@ module Engine
       @port = port
     end
 
+    def query path:nil, read_timeout:nil, **params
+      payload = get path:path, read_timeout:read_timeout, **params
+      post path:path, read_timeout:read_timeout, payload:payload
+    end
+
     def get path:nil, read_timeout:nil, **params
-      params&.map!{|k,v| "#{k}=#{CGI::escape v.to_s}"}&.join "&"
+      params   = params&.map{|k,v| "#{k}=#{CGI::escape v.to_s}"}&.join "&"
       request  = Net::HTTP::Get.new "#{path||"/"}?#{params}"
       response = Net::HTTP.start(@host, @port, read_timeout:read_timeout) do |http|
         http.request request
       end
       json = JSON.parse(response.body).symbolize_keys
-      json[:response].collect(&:symbolize_keys)
+      json[:response]&.collect &:symbolize_keys
     end
 
     def post path:nil, read_timeout:nil, payload:nil
