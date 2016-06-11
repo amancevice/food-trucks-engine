27e01556986d06313002bb3c983ae2da56fe9384
Alexander Mancevice <amancevice@cargometrics.com>
2016-06-11 11:38:29 -0400

    Added RoseKennedyGreenway source

--- lib/eater_atlas/service/source.rb
+++ lib/eater_atlas/service/source.rb
@@ -88,3 +88,41 @@ class CityOfBoston < HTMLSource
     end
   end
 end
+
+class RoseKennedyGreenway < HTMLSource
+  def response
+    script = super.xpath("//script").last
+    var    = script.text.strip.split(/\n/).first.strip
+    json   = JSON.parse var.sub(/\A\$(.*?) = (.*?);\z/, "{\"\\1\": \\2}")
+    json["trucksJSON"].map do |id, truck|
+      truck["locations"].map do |location|
+        location["days"].map do |day|
+          if Date.today.wday == day.to_i
+            date = Date.today
+          else
+            date = Chronic.parse("Next #{Date::DAYNAMES[day.to_i]}")
+          end
+          datetime = date.in_time_zone @timezone
+
+          meal  = Meal::LUNCH
+          delta = [meal.hours.first, meal.hours.last]
+          delta[-1] += 23 if delta.last < delta.first
+          start = (datetime + delta.first.hours).utc
+          stop  = (datetime + delta.last.hours).utc
+
+          { city:      @city,
+            endpoint:  @endpoint.to_s,
+            latitude:  nil,
+            longitude: nil,
+            place:     location["name"],
+            site:      truck["url"],
+            source:    self.class.to_s,
+            start:     start.to_s,
+            stop:      stop.to_s,
+            timezone:  @timezone,
+            truck:     truck["name"] }
+        end
+      end.flatten
+    end.flatten
+  end
+end
